941685029ec946fae33fbf1cad51b088d48bb672
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-08-08 23:11:16 +0530

    string functions

--- array50.java
+++ array50.java
@@ -10,7 +10,7 @@ public class array50{
         for(int i=0;i<b.length;i++){
             for(int j=0;j<b[i].length;j++){
                 System.out.print(b[i][j]+" ");
-            }
+            } 
             System.out.println();
         }
     }
