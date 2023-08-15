e664595678736c48a5ccb40930602256c7c1b2e4
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-08-15 17:47:25 +0530

    reverse words

---
+++ string59.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class string59{
+
+    public static boolean isPallindrome(StringBuilder str){
+        int i=0;
+        int j=str.length()-1;
+        boolean flag=true;
+        while(j>i){
+            if(str.charAt(i)==str.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string to be checked: ");
+        StringBuilder str=new StringBuilder(sc.nextLine());
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<str.length()+1;j++){
+                StringBuilder subStr=new StringBuilder(str.substring(i,j));
+                if(isPallindrome(subStr)){
+                    // System.out.println(subStr);
+                    count++;
+                }
+            }
+            
+        }
+        System.out.println("The number of pallindromic substrings are: "+count);
+    }
+}
