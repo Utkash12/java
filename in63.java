8f0ebe11a19e6de48405fb7b173386a8fea8b2fd
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-08-30 23:54:29 +0530

    program

--- in63.java
+++ in63.java
@@ -10,7 +10,7 @@ class shape{
 class cylinder extends shape{
     int height;
     // super(r);
-    cylinder(int h){
+    cylinder(int h){  
         super();
         System.out.println("I'm a parametrized constructor....");
         this.height=h;  
