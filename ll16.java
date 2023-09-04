b0f1cdbbbcd573730ebaac73a948ca7126e82e54
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-09-04 23:34:22 +0530

    reverse

---
+++ ll16.java
@@ -0,0 +1,38 @@
+public class ll16{
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static class linkedlist{
+        Node head=null;
+        Node tail=null;
+        void insertAtEnd(int val){
+            Node temp=new Node(val);
+            if(head==null){
+                head=temp;
+                tail=temp;
+            }
+            else{
+                tail.next=temp;
+                tail=temp;
+            }
+        }
+        void display(){
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+        }
+    }
+    public static void main(String[] args) {
+            linkedlist ll=new linkedlist();
+            ll.insertAtEnd(10);
+            ll.insertAtEnd(20);
+            ll.insertAtEnd(30);
+            ll.display();
+        }
+}
