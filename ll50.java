70c745044e52bf0c6cae12ff578e622d25d99903
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-09-09 23:42:48 +0530

    revision

---
+++ ll50.java
@@ -0,0 +1,43 @@
+public class ll50{
+    public static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            this.data=d;
+            this.next=null;
+        }
+    }
+    public static class linkedlist{
+        Node head=null;
+        Node tail=null;
+        void insertAtEnd(int val){
+            Node temp=new Node(val);
+            if(head==null){
+                head=temp;
+                tail=temp;
+            }
+            else{
+                tail.next=temp;
+                tail=temp;
+            }
+        }
+        void display(){
+            // Node temp=Node head;
+            Node temp=head;
+            while(temp!=null){
+                System.out.println(temp.data);
+                temp=temp.next;
+            }
+        }
+    }
+    public static void main(String[] args){
+        linkedlist ll=new linkedlist();
+        ll.insertAtEnd(5);
+        ll.insertAtEnd(10);
+        ll.insertAtEnd(20);
+        ll.insertAtEnd(30);
+        ll.insertAtEnd(10);
+        // Node temp=a;
+        ll.display();
+    }
+}
