79e995fddbbbb6f78e257639e5e16718973e7c1c
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-09-27 16:40:11 +0530

    stack

---
+++ Rextester.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+import java.lang.*;  
+public class Rextester  
+{    
+    public static void main(String args[])  
+    {  
+        do  
+        {  
+         System.out.print("javaTpoint");  
+         System.out.print(" ");  
+        }while(true);  
+    }  
+}  
---
+++ ll90.java
@@ -0,0 +1,40 @@
+// import java.util.Scanner;
+public class ll90{
+    public static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            this.data=d;
+            this.next=null;
+        }
+    }
+    public static class linkedlist{
+        Node head=null;
+        Node tail=null;
+        void insertAt(int val){
+            Node temp=new Node(val);
+            if(head==null){
+                head=temp;
+                tail=temp;
+            }
+            else{
+                tail.next=temp;
+                tail=temp;
+            }
+        }
+        void display(){
+            Node temp=head;
+            while(temp!=null){
+                System.out.println(temp.data);
+                temp=temp.next;
+            }
+        }
+    }
+    public static void main(String[] args){
+        linkedlist ll=new linkedlist();
+        ll.insertAt(10);
+        ll.insertAt(20);
+        ll.insertAt(30);
+        ll.display();
+    }
+}
