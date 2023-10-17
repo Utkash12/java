ffc9af6452f76bc42ab6d8f13225b7fffafdcbe7
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-10-17 13:03:47 +0530

    balanced

---
+++ ll160.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class ll160 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str=sc.nextLine();
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.size()>0 && ch==')'){
+                    if(st.peek()=='('){
+                        System.out.println("true");
+                        return;
+                    }
+                    else{
+                        while(st.peek()!='('){
+                            st.pop();
+                        }
+                        st.pop();
+                    }
+                }
+                else if(st.size()>0 && ch=='}'){
+                    if(st.peek()=='{'){
+                        System.out.println("true");
+                        return;
+                    }
+                    else{
+                        while(st.peek()!='{'){
+                            st.pop();
+                        }
+                        st.pop();
+                    }
+                }
+                else if(st.size()>0 && ch==']'){
+                    if(st.peek()=='['){
+                        System.out.println("true");
+                        return;
+                    }
+                    else{
+                        while(st.peek()!='['){
+                            st.pop();
+                        }
+                        st.pop();
+                    }
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+        System.out.println("false");
+
+    }
+}
