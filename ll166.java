42ecd629e507a899f3c6ddee64d4b30272cf1220
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-10-21 14:59:44 +0530

    stack

--- ll166.java
+++ ll166.java
@@ -1,5 +1,20 @@
 public class ll166{
     public static void main(String[] args) {
-        
+        int[] arr={1,3,7,5,4,8,2,3,1,4};
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>arr[i]){
+                    ans[i]=arr[j];
+                    break;
+                }
+                else{
+                    ans[i]=-1;
+                }
+            }
+        }
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
     }
 }
---
+++ ll167.java
@@ -0,0 +1,13 @@
+//next greater element
+public class ll167{
+    public static void main(String[] args){
+        int[] arr={1,3,7,5,4,8,2,3,1,4};
+        int k=0;
+        for(int i=0;i<arr.length;i++){
+            while
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
