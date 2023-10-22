9f99c019656d3e0b690b4190f9c51b6955192d11
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-10-22 12:29:43 +0530

    histogram

---
+++ ll171.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class ll171{
+    public static void main(String[] args){
+        int[] arr={5,2,4,6,3,5};
+        int[] ans=new int[arr.length];
+        Stack<Integer> st=new Stack<>();
+        ans[arr.length-1]=-1;
+        st.push(arr[arr.length-1]);
+        for(int i=arr.length-2;i>=0;i--){
+            while(st.size()>0 && st.peek()>arr[i]){
+                st.pop();
+            }
+            if(st.size()==0){
+                ans[i]=-1;
+            }
+            else{
+                ans[i]=st.peek();
+            }
+            st.push(arr[i]);
+        }
+        for(int i=0;i<ans.length;i++){
+            System.out.println(ans[i]);
+        }
+    }
+}
