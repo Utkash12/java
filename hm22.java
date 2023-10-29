c51ab03fa102247e4b2b51f85d51be9ade3a5f1b
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-10-29 16:37:28 +0530

    anagram

---
+++ hm22.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class hm22{
+    public static void main(String[] args){
+        HashMap<Character,Character> hm=new HashMap<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first string: ");
+        String str1=sc.nextLine();
+        System.out.println("Enter the second string: ");
+        String str2=sc.nextLine();
+        for(int i=0;i<str1.length();i++){
+            char ch1=str1.charAt(i);
+            char ch2=str2.charAt(i);
+            if(hm.containsKey(ch1)){
+                if(hm.get(ch1)!=ch2){
+                    System.out.println("false");
+                }
+            }
+            else if(hm.containsValue(ch2)){
+                System.out.println("false");
+            }
+            else{
+                hm.put(ch1,ch2);
+            }
+        }
+        System.out.println("true");
+    }
+}
