a6c86e81502dde48771b5132a2dcd7d52e2b46e5
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-11-02 22:35:58 +0530

    linear search

--- hs32.java
+++ hs32.java
@@ -1,25 +1,25 @@
 import java.util.*;
-public class hs31 {
+public class hs32{
     public static void main(String[] args){
-        int[] arr={1,6,4,7,3,5,9,10,11,12,13,14,15,16};
+        int[] arr={1,2,3,4,6,7,8,9,10,23,34,35,36};
         HashSet<Integer> hs=new HashSet<>();
         for(int i=0;i<arr.length;i++){
             hs.add(arr[i]);
         }
-        System.out.println(hs);
-        int currStreak;
-        int maxStresk=0;
+        int currStreak=0;
+        int maxStreak=0;
         for(int i=0;i<arr.length;i++){
             if(!hs.contains(arr[i]-1)){
                 currStreak=1;
                 int curr=arr[i];
-                while(hs.contains(curr+1)){
+                while(hs.contains(arr[i]+1)){
                     curr++;
                     currStreak++;
                 }
-                maxStresk=Math.max(maxStresk,currStreak);
+                maxStreak=Math.max(currStreak,maxStreak);
             }
+            
         }
-        System.out.println(maxStresk);
+        System.out.println(maxStreak);
     }
 }
