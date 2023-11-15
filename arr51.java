def2cb4a9ae289d0db8911766bdef0c3b52e8afc
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-11-15 23:58:23 +0530

    even odd

---
+++ arr51.java
@@ -0,0 +1,23 @@
+public class arr51{
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,8,9,10};
+        int left=0;
+        int right=arr.length-1;
+        while(left<right){
+            if(left%2==0){
+                left++;
+            }
+            else if(!(right%2==0)){
+                right--;
+            }
+            else{
+                arr[left]=arr[left]+arr[right];
+                arr[right]=arr[left]-arr[right];
+                arr[left]=arr[left]-arr[right];
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+"   ");
+        }
+    }
+}
