b1cde175e83575d5951a2f8bf17e1b460b8fae40
Utkarsh Sharma <164newshubhamcity@gmail.com>
2023-11-20 16:34:47 +0530

    insertion sort

--- arr87.java
+++ arr87.java
@@ -1,20 +1,32 @@
-//selection sort
-public class arr87{
-    public static void main(String[] args){
-        int[] arr={1,21,43,65,76,22,44,53};
-        int small=0;
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[small]){
-                    small=j;
-                }
-                int temp=arr[j];
-                arr[j]=arr[small];
-                arr[small]=temp;
-            }
-        }
-        for(int i=0;i<arr.length-1;i++){
-            System.out.print(arr[i]+"   ");
-        }
-    }
-}
+public class arr87 {  
+    public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
+       
+    public static void main(String a[]){  
+        int[] arr1 = {9,14,3,2,43,11,58,22};  
+        System.out.println("Before Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+        System.out.println();  
+          
+        selectionSort(arr1);//sorting array using selection sort  
+         
+        System.out.println("After Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+    }  
+}  
---
+++ arr88.java
@@ -0,0 +1,20 @@
+//selection sort
+public class arr88{
+    public static void main(String[] args){
+        int[] arr={1,21,43,65,76,22,44,53};
+        for(int i=0;i<arr.length-1;i++){
+            int small=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>arr[small]){
+                    small=j;
+                }
+                int temp=arr[j];
+                arr[j]=arr[small];
+                arr[small]=temp;
+            }
+        }
+        for(int i=0;i<arr.length-1;i++){
+            System.out.print(arr[i]+"   ");
+        }
+    }
+}
