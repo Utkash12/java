17165c83349257a32b872699d018e81b9079692c
Utkarsh Sharma <164newshubhamcity@gmail.com>
2024-02-26 09:23:06 +0530

    linkedlist

---
+++ p21.java
@@ -0,0 +1,47 @@
+public class p21 {
+    public static class linkedlist {
+        void insertAt(int data) {
+            Node head = null;
+            Node tail = null;
+            Node temp = new Node(data);
+            if (head == null) {
+                head = temp;
+                tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+        // void display(){
+        // Node temp=head;
+        // if(temp==null){
+        // System.out.println("No element in linkedlist");
+        // }
+        // else{
+        // while(temp!=null){
+        // System.out.println(temp.data);
+        // temp=temp.next;
+        // }
+        // }
+        // }
+    }
+
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            this.data = d;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        linkedlist ll = new linkedlist();
+        ll.insertAt(10);
+        ll.insertAt(20);
+        ll.insertAt(30);
+        ll.insertAt(40);
+        ll.display();
+    }
+}
--- p22.java
+++ p22.java
@@ -1,4 +1,4 @@
-public class p20 {
+public class p22 {
     public static class Node {
         int data;
         Node next;
@@ -8,7 +8,6 @@ public class p20 {
             this.next = null;
         }
     }
-
     public static class linkedlist {
         Node head = null;
         Node tail = null;
@@ -23,14 +22,6 @@ public class p20 {
                 tail = temp;
             }
         }
-        void delete(){
-            Node temp=head;
-            while(temp.next.next!=null){
-                temp=temp.next;
-            }
-            temp.next=null;
-            tail=temp;
-        }
         void display() {
             Node temp = head;
             while (temp != null) {
@@ -38,40 +29,13 @@ public class p20 {
                 temp = temp.next;
             }
         }
-
-        void insert(int data) {
-            Node temp = new Node(data);
-            if (head == null) {
-                head = temp;
-                tail = temp;
-            } else {
-                temp.next = head;
-                head = temp;
-            }
-        }
-        void del(){
-            head=head.next;
-            while(head!=null){
-                System.out.println(head.data);
-                head=head.next;
-            }
-        }
     }
-
     public static void main(String[] args) {
         linkedlist ll = new linkedlist();
         ll.insertAt(10);
         ll.insertAt(20);
         ll.insertAt(30);
         ll.insertAt(40);
-        ll.insert(1);
-        ll.insert(-1);
-        ll.display();
-        ll.delete();
-        System.out.println("After deletion");
-        ll.display();
-        System.out.println("After deletion");
-        ll.del();
         ll.display();
     }
 }
