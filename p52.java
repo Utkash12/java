37c1c53aee2512e638e861efe97f9dbdc104c7e4
Utkarsh Sharma <164newshubhamcity@gmail.com>
2024-03-10 15:11:29 +0530

    stack

---
+++ p52.java
@@ -0,0 +1,36 @@
+public class p52 {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            this.data = d;
+            this.next = null;
+        }
+    }
+
+    public static Node middle(Node head) {
+        Node fast = head;
+        Node slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = null;
+        Node temp = middle(a);
+        System.out.println(temp.data);
+    }
+}
