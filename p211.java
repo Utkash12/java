5bd911898bccc83dc4fe92c0d4ccedc4b6d9d8c6
Utkarsh Sharma <164newshubhamcity@gmail.com>
2024-07-01 13:01:50 +0530

    inheritance

--- p211.java
+++ p211.java
@@ -1,3 +1,19 @@
-public class p211 {
-    
+interface draw{
+    void drawing();
+}
+interface print{
+    void printing();
+}
+public class p211 implements draw,print{
+    public void drawing(){
+        System.out.println("Drawing...");
+    }
+    public void printing(){
+        System.out.println("Printing...");
+    }
+    public static void main(String[] args){
+        p211 p=new p211();
+        p.drawing();
+        p.printing();
+    }
 }
